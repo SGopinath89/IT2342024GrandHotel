01370807d5fc9d13ca2ba8fd02d5910614b36ab3
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-05-12 17:22:58 +0530

    admin front end
    order list,list items and add item pages  basic structure.

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+import { Link, NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div class='navbar'>
+        <img className='logo' src={assets.logo} alt="" />
+        <div className="nav_links">
+        <NavLink to='/Orders' className='nav-link'>Orders</NavLink>
+        <NavLink to='/List' className='nav-link'>List Items</NavLink>
+        <NavLink to='/Add' className='nav-link'>Add Items</NavLink>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
