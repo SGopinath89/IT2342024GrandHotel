33467b8e28a4120ed6db796861e3c9cf9229424c
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-05-23 09:18:15 +0530

    user login backend
    user can log using him email and password .

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,31 @@ const validator=require('validator')
 //login
 
 const loginUser=async (req,res) =>{
+    const {email,password}=req.body;
+    try{
 
+        const user =await userModel.findOne({email})
+
+        if(!user){
+            return res.json({success:false,message:"user doesn't exits"})
+     }
+
+     const isMatch =await bcrypt.compare(password,user.password)
+
+     
+
+    if(!isMatch){
+        return res.json({success:false,message:"invalid userlogin"})
+    }
+
+    const token=createToken(user._id)  
+    res.json({success:true,token })
+    }
+    catch(error){
+        console.log(error)
+        res.json({success:false,message:"Error"})
+
+    }
 
 
 
