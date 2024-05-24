12fd251e25dbf67af733376a1c661024a85c3fbd
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-05-25 00:39:42 +0530

    menu filter and cart options
    cart add,remove fuctions

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,20 +1,34 @@
-import React,{useContext} from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../context/StoreContext'
-import FoodItems from '../FoodItems/FoodItems'
+import React, { useContext } from 'react';
+import './FoodDisplay.css';
+import { StoreContext } from '../../context/StoreContext';
+import FoodItems from '../FoodItems/FoodItems';
 
-const FoodDisplay=(category)=> {
-    const {food_list}=useContext(StoreContext)
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+  
   return (
     <div className='food-display' id='food-display'>
-        <h2>oreder your favorite food</h2>
-        <din className="food-display-list">
-            {food_list.map((item,index)=>{
-                return <FoodItems key={index} id={item._id}  name={item.name} description={item.description} price={item.price} image={item.image}/>
-            })}
-        </din>
+      <h2>Order Your Favorite Food</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          console.log(category, item.category);
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItems
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+          return null;
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default FoodDisplay
+export default FoodDisplay;
--- frontend/src/components/FoodItems/FoodItems.jsx
+++ frontend/src/components/FoodItems/FoodItems.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import './FoodItems.css';
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 
 const FoodItems = ({ id, name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0);
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
         <img className='food-item-image' src={image} alt={name} />
-        {!itemCount ? (
+        {!cartItems[id] ? (
           <img
             className="add"
-            onClick={() => setItemCount(prev => prev + 1)}
+            onClick={() => addToCart(id)}
             src={assets.add_icon_white}
             alt="Add"
           />
         ) : (
           <div className='food-item-counter'>
             <img
-              onClick={() => setItemCount(prev => prev - 1)}
+              onClick={() => removeFromCart(id)}
               src={assets.remove_icon_red}
               alt="Remove"
             />
-            <p>{itemCount}</p>
+            <p>{cartItems[id]}</p>
             <img
-              onClick={() => setItemCount(prev => prev + 1)}
+              onClick={() => addToCart(id)}
               src={assets.add_icon_green}
               alt="Add"
             />
@@ -42,6 +43,6 @@ const FoodItems = ({ id, name, price, description, image }) => {
       </div>
     </div>
   );
-}
+};
 
 export default FoodItems;
