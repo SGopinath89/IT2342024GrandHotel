42ce20649f42bce79a63bcf03228135a1d11f17d
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-05-28 11:11:28 +0530

    login,signup frontend
    popup for login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 import Cart from './pages/cart/cart';
+import LoginPopup from './components/LoginPopup/LoginPopup';
 const App = () => {
+  const [showLogin,setShowLogin]=useState(false)
   return (
+    <>
+    {showLogin?<LoginPopup  setShowLogin={setShowLogin}/>:<></>}
     
       <div className='app'>
-        <Navbar/>
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path='/home' element={<Home/>} />
           <Route path='/cart' element={<Cart/>} />
         </Routes>
         
       </div>
-    
+    </>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,20 +3,20 @@ import './Navbar.css'
 import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
     const [menu,setMenu]=useState("home")
   return (
     
     <div class='navbar'>
         <img className='logo' src={assets.logo} alt="" />
         <ul className="navbar-menu">
-        <li onClick={()=>setMenu("home")}className={menu==="home"?"active":""}><Link to='/Home'>Home</Link></li>
-        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}><Link to='/menue'>Menu</Link></li>
-        <li onClick={()=>setMenu("about-us")} className={menu==="about-us"?"active":""}><Link to='/about-us'>About us</Link></li>
+        <a href='' onClick={()=>setMenu("home")}className={menu==="home"?"active":""}><Link to='/Home'>Home</Link></a>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}><Link to='#explore-menu'>Menu</Link></a>
+        <a href='' onClick={()=>setMenu("about-us")} className={menu==="about-us"?"active":""}><Link to='/about-us'>About us</Link></a>
         </ul>
         <div className='navbar-right'>
             <img src={assets.basket_icon} alt=''/>
-            <button>sign in</button>
+            <button onClick={()=>setShowLogin(true)}>sign in</button>
 
         </div>
     </div>
