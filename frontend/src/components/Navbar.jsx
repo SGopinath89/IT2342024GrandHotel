ff18f33b60788cd85bb567bc37c86089f37872e7
isulnethila <139636196+isulnethila@users.noreply.github.com>
2024-06-05 00:19:12 +0530

    login and logout function

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 import Cart from '../pages/Cart/Cart'
+import { StoreContext } from '../context/StoreContext'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = ({setShowLogin}) => {
     const [menu,setMenu]=useState("home")
+    const{token,setToken}=useContext(StoreContext)
+    
+    const navigate=useNavigate();
+    
+    const logout=()=>{
+      localStorage.removeItem("token")
+      setToken("")
+      navigate("/home")
+
+
+    }
   return (
     
     <div class='navbar'>
@@ -16,8 +29,19 @@ const Navbar = ({setShowLogin}) => {
         <a href='' onClick={()=>setMenu("about-us")} className={menu==="about-us"?"active":""}><Link to='/about-us'>About us</Link></a>
         </ul>
         <div className='navbar-right'>
+          
             <Link to='/cart'><img src={assets.basket_icon} alt=''/></Link>
-            <button onClick={()=>setShowLogin(true)}>sign in</button>
+            {!token?<button onClick={()=>setShowLogin(true)}>sign in</button>
+            :<div className='navbar-profile'>
+                <img src={assets.profile_icon} alt=""/>
+                <ul className="nav-profile-dropdown">
+                  <li><img src={assets.bag_icon} alt=""/><p>Orders</p></li>
+                  <hr />
+                  <li onClick={logout}><img src={assets.logout_icon} alt=""/><p>Logout</p></li>
+                </ul>
+              </div>}
+
+            
 
         </div>
     </div>
